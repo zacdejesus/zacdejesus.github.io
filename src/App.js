0740966c51c3e2fc8fb73225121da9d845b1e6fe
Alejandro De Jesus <zadkielsi@outlook.com>
2021-10-23 22:54:14 -0300

    testimonial added

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Header from './Components/Header';
 import Footer from './Components/Footer';
 import About from './Components/About';
 import Resume from './Components/Resume';
+import Testimonials from './Components/Testimonials';
 
 class App extends Component {
 
@@ -46,6 +47,7 @@ class App extends Component {
         <Header data={this.state.resumeData.main}/>
         <About data={this.state.resumeData.main}/>
         <Resume data={this.state.resumeData.resume}/>
+        <Testimonials data={this.state.resumeData.testimonials}/>
         <Footer data={this.state.resumeData.main}/>
       </div>
     );
